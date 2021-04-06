fc95bed6fd24b4d5a2618d751958a30e599c5a69
Durgesh0694 <durgesh.kumar.mnnit@gmail.com>
2021-04-06 13:35:57 +0530

    update settings keys

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(y+0irow+n&wi=$=)ca=eb9rzk$u9gvp^=zwp^5-&#&v4y537y'
-
+# SECRET_KEY = '(y+0irow+n&wi=$=)ca=eb9rzk$u9gvp^=zwp^5-&#&v4y537y'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'newtechblogg.herokuapp.com'
+]
 
 
 # Application definition
