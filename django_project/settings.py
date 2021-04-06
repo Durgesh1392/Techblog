726229535c1865c1decac953cf931be84b677af4
Durgesh0694 <durgesh.kumar.mnnit@gmail.com>
2021-04-06 14:01:24 +0530

    update postgres set

--- django_project/settings.py
+++ django_project/settings.py
@@ -157,4 +157,5 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 #AWS_QUERYSTRING_AUTH=False  # for keys not going in url
+
 django_heroku.settings(locals())
